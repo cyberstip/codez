431f5bd9ebfb0ddde25ea70365547adb403b3999
Mike Stipicevic <stip@chromium.org>
2015-01-05 16:17:35 -0800

    awesome

--- zipkin_upload/process.py
+++ zipkin_upload/process.py
@@ -57,8 +57,9 @@ class JobUpdate(object):
     return 'JobUpdate: %s' % repr(self.job_dict)
 
   def zipkin_data(self):
+    # self.rietveld_timestamp, useless?
     return {
-        'ss': self.rietveld_timestamp,
+        'ss': self.timestamp,
         'cr': self.timestamp,
         'host': self.job_dict['slave'],
         'service_name': builder_id(
@@ -74,7 +75,7 @@ class VerifierJobsUpdate(CQApiRecord):
     for master in self._fields['jobs'].itervalues():
       for builder in master.itervalues():
         for job in builder['rietveld_results']:
-          if job['result'] != -1:
+          if job['result'] != -1 and job['buildnumber'] is not None:
             self.job_updates.add(JobUpdate(
                 self.timestamp,
                 job,
@@ -89,6 +90,10 @@ class VerifierStart(CQApiRecord):
     for mastername, master_builders in self._fields['tryjobs'].iteritems():
       for builder in master_builders.keys():
         self.tryjobs.add(builder_id(mastername, builder))
+
+
+class VerifierRetry(CQApiRecord):
+  pass
 
 
 def return_new_jobs(jobs_updates):
@@ -116,6 +121,29 @@ def chunk_attempts(records):
         current_chunk = None
     elif current_chunk is not None:
       current_chunk.append(record)
+
+
+def translate_retry_to_start(records):
+  last_update = None
+  for record in records:
+    if isinstance(record, VerifierRetry):
+      assert last_update is not None
+      bad_jobs = {}
+      for mastername, master in last_update._fields['jobs'].iteritems():
+        for buildername, builder in master.iteritems():
+          for job in builder['rietveld_results']:
+            if job['result'] != 0:
+              bad_jobs.setdefault(mastername, {})[buildername] = {}
+      yield VerifierStart(
+          timestamp=datetime_to_timestamp(record.timestamp),
+          tags=record._tags,
+          key=record._key,
+          fields={'tryjobs': bad_jobs})
+    elif isinstance(record, VerifierJobsUpdate):
+      last_update = record
+      yield record
+    else:
+      yield record
 
 
 def link_jobs_to_trigger(records):
@@ -150,6 +178,7 @@ class PatchStop(CQApiRecord):
 RECORD_TYPES = {
     'verifier_jobs_update': VerifierJobsUpdate,
     'verifier_start': VerifierStart,
+    'verifier_retry': VerifierRetry,
     'patch_start': PatchStart,
     'patch_stop': PatchStop,
 }
@@ -161,7 +190,7 @@ def constructor(record):
 
 
 def gen_zipkin_uuid():
-  return uuid.uuid4().hex[0:16]
+  return uuid.uuid4().hex[0:15]
 
 
 def datetime_to_timestamp(dt):
@@ -169,7 +198,8 @@ def datetime_to_timestamp(dt):
 
 
 class ZipkinSpan(object):
-  def __init__(self, name, trace_id, span_id, parent_id, annotations):
+  def __init__(
+      self, name, trace_id, span_id, parent_id, parent_name, annotations):
     self.name = name
     self.trace_id = trace_id
     self.span_id = span_id
@@ -177,13 +207,30 @@ class ZipkinSpan(object):
     self.annotations = []
 
     for annot_name, annot_value in annotations.iteritems():
-      if annot_name in ('cs', 'cr', 'ss', 'sr'):
+      if annot_name in ('cs', 'cr'):
+        self.annotations.append({
+            'key': annot_name,
+            'type': 'timestamp',
+            'value': int(datetime_to_timestamp(annot_value) * 1000000),
+            'name': parent_name,
+            'host': {
+                'ipv4': '10.10.10.10',
+                'port': 1234,
+                'service_name': parent_name,
+            }
+        })
         # timestamps are integers in microseconds
+      elif annot_name in ('ss', 'sr'):
         self.annotations.append({
             'key': annot_name,
             'type': 'timestamp',
             'value': int(datetime_to_timestamp(annot_value) * 1000000),
             'name': self.name,
+            'host': {
+                'ipv4': '10.10.10.10',
+                'port': 1234,
+                'service_name': self.name,
+            }
         })
 
   def render(self):
@@ -204,20 +251,25 @@ def construct_spans(records):
   records = list(records)
   trace_id = gen_zipkin_uuid()
 
-  root_span = ZipkinSpan('cq_request', trace_id, trace_id, None, {
+  root_span = ZipkinSpan('cq_service', trace_id, trace_id, None, 'cq_request', {
       'sr': records[0].timestamp,
       'ss': records[-1].timestamp,
+      'cs': records[0].timestamp,
+      'cr': records[-1].timestamp,
   })
   spans = [root_span]
   for record in records[1:-1]:
     spans.append(ZipkinSpan(
-        record['service_name'], trace_id, gen_zipkin_uuid(), trace_id, record))
+        record['service_name'],
+        trace_id, gen_zipkin_uuid(), trace_id, 'cq_service', record))
 
   return spans
 
 
 def send_to_restkin(url, spans):
   headers = {'content-type': 'application/json'}
+  with open('bruh.json', 'w') as f:
+    json.dump(spans, f, indent=2)
   r = requests.post(url, data=json.dumps(spans), headers=headers)
   print r
 
@@ -241,10 +293,15 @@ def main():
     items = json.load(f)
 
   sorted_records = sorted(filter(bool, map(constructor, items['results'])))
+  print sorted_records[0].timestamp
+  print datetime_to_timestamp(sorted_records[0].timestamp)
+  print sorted_records[-1].timestamp
+  print datetime_to_timestamp(sorted_records[-1].timestamp)
   spans = []
-  for record_set in chunk_attempts(sorted_records):
+  for record_set in list(chunk_attempts(sorted_records))[0:1]:
     for span in construct_spans(
-        link_jobs_to_trigger(return_new_jobs(record_set))):
+        link_jobs_to_trigger(translate_retry_to_start(
+            return_new_jobs(record_set)))):
       spans.append(span.render())
   send_to_restkin(args.restkin_url, spans)
 
